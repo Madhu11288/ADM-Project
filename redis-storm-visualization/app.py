8381af305f121bd124fa6b01e8ce4a422a30abc8
Sharanya Bathey <sharanya.bathey@gmail.com>
2015-04-17 23:04:11 -0500

    Basic visualization for queries

--- redis-storm-visualization/app.py
+++ redis-storm-visualization/app.py
@@ -1,31 +1,41 @@
 from flask import Flask, Response, render_template
 from rediscluster import RedisCluster
 import time
+import json
 
 app = Flask(__name__)
 startup_nodes = [{"host": "10.0.0.30", "port": "7000"}]
 redis = RedisCluster(startup_nodes=startup_nodes, decode_responses=True)
 
 
-def trending_tweets():
+def trending_hash_tags():
     trending_hashtags_data = redis.zrange("trending-topics", -20, -1)
-    trending_hashtags = []
+    trending_hashtags = ""
     for hashtag in trending_hashtags_data:
-        trending_hashtags.append(hashtag.split(":")[1])
-    print(trending_hashtags)
-    yield 'trending_tweets: %s\n\n' % trending_hashtags
+        key = hashtag.split(":")[1]
+        value = redis.get(hashtag)
+        result = str(key) + "|" + str(value) + "|%*%|"
+        trending_hashtags += result
+    yield 'data: %s\n\n' % trending_hashtags[0:(len(trending_hashtags)-5)]
 
 
 def tweets_sliding_window():
     current_time_milliseconds = time.time() * 1000
-    five_mins_past = 0.5 * 60 * 1000
-    time_five_mins_ago_milliseconds = current_time_milliseconds - five_mins_past
-    tweet_ids = redis.zrangebyscore("tweet-time-series", int(time_five_mins_ago_milliseconds),
-                                 int(current_time_milliseconds))
-
-    tweets = redis.mget(tweet_ids)
-    print(tweets)
-    yield 'tweets: %s\n\n' % tweets
+    five_mins_past = 30 * 1000
+    print(five_mins_past)
+    time_five_mins_ago_milliseconds = (current_time_milliseconds - five_mins_past)
+    print(round(time_five_mins_ago_milliseconds))
+    print(round(current_time_milliseconds))
+    tweet_ids = redis.zrangebyscore("tweet-time-series",
+                                 round(time_five_mins_ago_milliseconds - five_mins_past),
+                                 round(time_five_mins_ago_milliseconds))
+    print(tweet_ids)
+    tweets_data = redis.mget(tweet_ids)
+    print(tweets_data)
+    tweets = ""
+    for tweet in tweets_data:
+        tweets = tweets + str(tweet) + "|%*%|"
+    yield 'data: %s\n\n' % tweets[0:(len(tweets)-4)]
 
 
 @app.route('/')
@@ -33,9 +43,9 @@ def show_homepage():
     return render_template("index.html")
 
 
-@app.route('/trending-hashtags-stream')
-def hashtags_stream():
-    return Response(trending_tweets(), mimetype="text/event-stream")
+@app.route('/hashtags-stream')
+def trending_stream():
+    return Response(trending_hash_tags(), mimetype="text/event-stream")
 
 
 @app.route('/tweets-stream')
