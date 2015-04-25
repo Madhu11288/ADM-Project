9d5edf851a531133c5575cbfdf51a93e7e087466
Madhushree Sreenivasa <s.madhushree@gmail.com>
2015-04-24 20:03:57 -0500

    linear road topology

--- storm-aerospike/src/main/java/com/stormspike/linear/road/schema/VehicleInLastFiveMinsTable.java
+++ storm-aerospike/src/main/java/com/stormspike/linear/road/schema/VehicleInLastFiveMinsTable.java
@@ -3,6 +3,8 @@ package com.stormspike.linear.road.schema;
 
 import com.aerospike.client.*;
 import com.aerospike.client.policy.WritePolicy;
+import com.aerospike.client.query.IndexType;
+import com.aerospike.client.task.IndexTask;
 import com.stormspike.topology.Constants;
 
 import java.util.ArrayList;
@@ -17,24 +19,29 @@ public class VehicleInLastFiveMinsTable {
         writePolicy = new WritePolicy();
     }
 
-    public static void main(String[] args) {
-        VehicleInLastFiveMinsTable vilfm = new VehicleInLastFiveMinsTable();
-        vilfm.writeVehicleList();
-    }
+    public void writeVehicleList(String xWay, String lane, String direction, String segment, String vehicleId,String time) {
+        IndexTask indexTask = this.client.createIndex(writePolicy, Constants.AS_NAMESPACE, Constants.AS_VEHICLE_LIST_SET, "vehicleid1",Constants.VEHICLE_ID_BIN, IndexType.STRING);
+        indexTask.waitTillComplete();
 
-    private void writeVehicleList() {
-        int xway = 1;
-        int lane = 1;
-        int direction = 1;
-        int segment = 1;
-        int vehicleId = 10001;
-        String keyId = String.valueOf(xway)+String.valueOf(lane)+String.valueOf(direction)+String.valueOf(segment);
+        IndexTask indexTask1 = this.client.createIndex(writePolicy, Constants.AS_NAMESPACE, Constants.AS_VEHICLE_LIST_SET, "xWay",Constants.XWAY_BIN, IndexType.STRING);
+        indexTask1.waitTillComplete();
+
+        IndexTask indexTask2 = this.client.createIndex(writePolicy, Constants.AS_NAMESPACE, Constants.AS_VEHICLE_LIST_SET, "lane",Constants.LANE_BIN, IndexType.STRING);
+        indexTask2.waitTillComplete();
+
+        IndexTask indexTask3 = this.client.createIndex(writePolicy, Constants.AS_NAMESPACE, Constants.AS_VEHICLE_LIST_SET, "direction",Constants.DIRECTION_BIN, IndexType.STRING);
+        indexTask3.waitTillComplete();
+
+        IndexTask indexTask4 = this.client.createIndex(writePolicy, Constants.AS_NAMESPACE, Constants.AS_VEHICLE_LIST_SET, "segment",Constants.SEGMENT_BIN, IndexType.STRING);
+        indexTask4.waitTillComplete();
+
+        String keyId = xWay + lane + direction + segment;
         Key key = new Key(Constants.AS_NAMESPACE, Constants.AS_VEHICLE_LIST_SET, keyId);
-        Bin bin1 = new Bin(Constants.XWAY_BIN, 0);
-        Bin bin2 = new Bin(Constants.LANE_BIN, 1);
-        Bin bin3 = new Bin(Constants.DIRECTION_BIN, 0);
-        Bin bin4 = new Bin(Constants.SEGMENT_BIN, 0);
-        Bin bin5 = new Bin(Constants.TIME_BIN, 0);
+        Bin bin1 = new Bin(Constants.XWAY_BIN, xWay);
+        Bin bin2 = new Bin(Constants.LANE_BIN, lane);
+        Bin bin3 = new Bin(Constants.DIRECTION_BIN, direction);
+        Bin bin4 = new Bin(Constants.SEGMENT_BIN, segment);
+        Bin bin5 = new Bin(Constants.TIME_BIN, time);
         Bin bin6;
 
         Record record = this.client.get(this.writePolicy, new Key(Constants.AS_NAMESPACE, Constants.AS_VEHICLE_LIST_SET, Value.get(keyId)), Constants.VEHICLE_LIST_BIN);
@@ -44,13 +51,12 @@ public class VehicleInLastFiveMinsTable {
                 vehicleList.add(vehicleId);
             }
             bin6 = new Bin(Constants.VEHICLE_LIST_BIN, Value.get(vehicleList));
-            this.client.put(this.writePolicy, key, bin1, bin2, bin3, bin4, bin5,bin6);
+            this.client.put(this.writePolicy, key, bin1, bin2, bin3, bin4, bin5, bin6);
         } else{
             ArrayList vehicleList = new ArrayList();
             vehicleList.add(vehicleId);
             bin6 = new Bin(Constants.VEHICLE_LIST_BIN,Value.get(vehicleList));
-            this.client.put(writePolicy, key,  bin1, bin2, bin3, bin4, bin5,bin6);
+            this.client.put(writePolicy, key,  bin1, bin2, bin3, bin4, bin5, bin6);
         }
     }
-
 }
---
+++ storm-aerospike/src/main/java/com/stormspike/spout/LinearRoadSpout.java
@@ -0,0 +1,96 @@
+package com.stormspike.spout;
+
+import backtype.storm.spout.SpoutOutputCollector;
+import backtype.storm.task.TopologyContext;
+import backtype.storm.topology.IRichSpout;
+import backtype.storm.topology.OutputFieldsDeclarer;
+import backtype.storm.tuple.Fields;
+import backtype.storm.tuple.Values;
+import com.aerospike.client.AerospikeClient;
+import com.aerospike.client.policy.RecordExistsAction;
+import com.aerospike.client.policy.WritePolicy;
+import com.stormspike.topology.Constants;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Map;
+
+public class LinearRoadSpout implements IRichSpout {
+
+    SpoutOutputCollector _collector;
+    AerospikeClient aerospikeClient;
+    BufferedReader br;
+    Process process;
+    WritePolicy aerospikeWritePolicy;
+
+    @Override
+    public void declareOutputFields(OutputFieldsDeclarer declarer) {
+        declarer.declare(new Fields("entry"));
+    }
+
+    @Override
+    public Map<String, Object> getComponentConfiguration() {
+        return null;
+    }
+
+    @Override
+    public void open(Map conf, TopologyContext context, SpoutOutputCollector collector) {
+        _collector = collector;
+
+        this.aerospikeClient = new AerospikeClient(Constants.AEROSPIKE_HOST, Constants.AEROSPIKE_PORT);
+        this.aerospikeWritePolicy = new WritePolicy();
+        this.aerospikeWritePolicy.maxRetries = 10;
+        this.aerospikeWritePolicy.recordExistsAction = RecordExistsAction.UPDATE;
+
+        String command = "/Users/madhushrees/ADM_/datadriver/datafeeder " +
+                "/Users/madhushrees/ADM_/datadriver/datafile3hours.dat";
+        try {
+            process = Runtime.getRuntime().exec(command);
+            br = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void close() {
+        try {
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void activate() {
+
+    }
+
+    @Override
+    public void deactivate() {
+
+    }
+
+    @Override
+    public void nextTuple() {
+        String line;
+        try {
+            while ((line = br.readLine()) != null) {
+                this._collector.emit(new Values(line));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void ack(Object msgId) {
+
+    }
+
+    @Override
+    public void fail(Object msgId) {
+
+    }
+}
